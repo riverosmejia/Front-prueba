222ed6c30c17ff3f31031a3a2ac043b078ebbd2f
miguel <riverosmejiamiguel@gmail.com>
2025-03-29 20:01:36 -0500

    aaa

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,15 +1,13 @@
 import styles from '../../styles/home.module.css'
+import Link from 'next/link'
 
 function Home() {
-
   return (
    <>
-
-<div className={styles.container}>
-   <h1 className={styles.title}>Home</h1>
-
-   <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, quo nulla? Illo eaque, voluptatum commodi cum neque, dolore possimus nihil nostrum molestiae dignissimos earum architecto facere, magni consectetur labore ipsum?</p>
-</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Home</h1>
+      <p className={styles.text}>Primera vez que entras? <Link href="/register"><a>Regístrate</a></Link></p>
+    </div>
    </>
   )
 }
